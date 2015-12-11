81e64033fa8e32c395d18dbe04ce668484e585b3
carrie <742305220@qq.com>
2015-12-11 15:56:51 +0800

    we can get the article,title,mp3 and picture by this python file

--- Story.py
+++ Story.py
@@ -6,30 +6,33 @@ from urllib import urlopen,urlretrieve
 
 class PatStory():
 
-    # 获取可可英语网小故事背诵达人1-37页的网页链接
     def getPageUrl(self):
         page_list = []
         page_list.append('http://www.kekenet.com/menu/13407/')
-        for i in range(35,37):
+        for i in range(1,37):
             http = 'http://www.kekenet.com/menu/13407/List_%s' % i + '.shtml'
-        # print http
+            # print http
             page_list.append(http)
+        print 1, page_list
         return page_list
 
-    # 获取每个故事的网页链接
+
     def getStoryUrl(self):
         story_list = []
         page_list = self.getPageUrl()
+        # print 2, page_list
         for page in page_list:
             text = urlopen(page).read()
+            text = str(text).replace('\r\n','')
             b = re.compile(r'http://www.kekenet.com/menu/2.*?shtml')
             list = re.findall(b, text)
-            # print list
+            # print 3, list
             for story in list:
                 story_list.append(story)
+        print 4, story_list
         return story_list
 
-    # 获取每个故事页面的源代码
+
     def getStorySC(self):
         sc_list = []
         story_list = self.getStoryUrl()
@@ -37,9 +40,10 @@ class PatStory():
             text = urlopen(story).read()
             text = str(text).replace('\r\n', '')
             sc_list.append(text)
+        # print 5, sc_list
         return sc_list
 
-    #获取每个故事的内容和标题
+
     def getContentAndTitle(self):
         content_list = []
         title_list = []
@@ -59,9 +63,10 @@ class PatStory():
                 essay = essay + '\r\n' + item
             content_list.append(essay)
         contentAndTitle_dict = {'content':content_list,'title':title_list}
+        print 6, contentAndTitle_dict
         return contentAndTitle_dict
 
-    #获取每个故事的MP3
+
     def getMP3Url(self):
         mp3_list = []
         mp3Url_list = []
@@ -76,21 +81,22 @@ class PatStory():
                 mp3Url_list.append('')
             else:
                 mp3Url_list.append(a.group())
+        print 7, mp3Url_list,len(mp3Url_list)
         return mp3Url_list
 
-    #下载MP3到本地
+
     def getMP3(self):
         i = 0
         mp3Url_list = self.getMP3Url()
         for mp3 in mp3Url_list:
             if mp3 != []:
-                a = r'/home/carrie/downloads/story/mp3/' + i + r'.mp3'
+                a = r'/home/carrie/downloads/story/mp3/' + str(i) + r'.mp3'
                 urlretrieve(mp3,a)
             i += 1
 
-    #获取每个故事的图片
+
     def getPictureUrl(self):
-        sc_list = []
+        sc_list = self.getStorySC()
         picture_list = []
         for sc in sc_list:
             pic_download = re.search(r'http://pic.kekenet.*?jpg', sc)
@@ -108,27 +114,29 @@ class PatStory():
                         pic_download = a[0]
             else:
                 pic_download = ''
-            print pic_download
+            print 8, pic_download
             picture_list.append(pic_download)
         return picture_list
 
-    #下载图片到本地
+
     def getPicture(self):
         i = 0
-        picture_list = self.getPicture()
+        picture_list = self.getPictureUrl()
         for picture in picture_list:
             if picture != '':
                 if re.findall('png',picture) == []:
-                    a = r'/home/carrie/downloads/story/picture/' + i + r'.jpg'
+                    a = r'/home/carrie/downloads/story/picture/' + str(i) + r'.jpg'
                     urlretrieve(picture,a)
                 else:
-                    a = r'/home/carrie/downloads/story/picture/' + i + r'.png'
+                    a = r'/home/carrie/downloads/story/picture/' + str(i) + r'.png'
                     urlretrieve(picture,a)
             i += 1
 
 
 if __name__ == '__main__':
     a = PatStory()
+    # a.getStoryUrl()
     a.getContentAndTitle()
+    # a.getMP3Url()
     a.getMP3()
     a.getPicture()
