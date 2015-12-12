b32d26ecc7f126a30ad8185cbb6124281b3488a5
carrie <742305220@qq.com>
2015-12-12 13:35:43 +0800

    change

--- Test.py
+++ Test.py
@@ -6,42 +6,38 @@ from urllib import urlopen,urlretrieve
 
 
 #获取"小故事背诵"网页链接
-page_list = []
-page_list.append('http://www.kekenet.com/menu/13407/')
-for i in range(33,34):
-    http = 'http://www.kekenet.com/menu/13407/List_%s' % i + '.shtml'
-    # print http
-    page_list.append(http)
-#
-#
-# 获取 每个故事 链接
+# page_list = []
+# page_list.append('http://www.kekenet.com/Article/yule/fangtanlu/')
+# for i in range(16,17):
+#     http = 'http://www.kekenet.com/Article/yule/fangtanlu/List_' + str(i) + '.shtml'
+#     print http
+#     page_list.append(http)
+# print len(page_list)
+# #
+# #
+# # 获取 每个故事 链接
 story_list = []
-for page in page_list:
-    text = urlopen(page).read()
-#     c = chardet.detect(text)
-#     code = c['encoding']
-#     text = str(text).decode(code, 'ignore').encode('utf-8').replace('\r\n', '')#把文本转换成字符串,并将换行符删去
-    # print text
-    # a = re.compile(u'<ul id="menu-list">(.*?)<div class="page th">')
-    # text = a.search(text)
-    # text = text.group()
-    # print type(text)
-    # print text
-    b = re.compile(r'http://www.kekenet.com/menu/2.*?shtml')
-    list = re.findall(b, text)
-    # print list
-    for story in list:
-        story_list.append(story)
+title_list = []
+# for page in page_list:
+text = urlopen('http://www.kekenet.com/Article/yule/fangtanlu/List_4.shtml').read()
+text = str(text).replace('\r\n', '')
+b = re.compile(r'http://www.kekenet.com/Article/2.*?shtml')
+a = re.compile(r'http://www.kekenet.com/Article/2.*?</a>')
+list = re.findall(b, text)
+list1 = re.findall(a,text)
+# print list
+for story in list:
+    story_list.append(story)
+    # print story
+for title in list1:
+    title = title.replace('&#8226;','•')
+    title = re.findall(r'>(.*?)<', title)
+    title_list.append(title[0])
+    print 2,title[0]
 # print len(story_list)
 
-
-# c = urlretrieve('http://pic.kekenet.com/2013/0830/83321377852882.JPG', '/home/carrie/66171411122.jpg')
-# # print c
-# c = urlretrieve('http://pic.kekenet.com//2014/0520/45641400576355.png','/home/carrie/66171411112.png')
-# c = urlretrieve('http://pic.kekenet.com//2014/0627/99731403860110.jpeg','/home/carrie/661714111177.jpg')
-
 # 获取 每篇文章的标题,内容,音频,图片
-# text = urlopen('http://www.kekenet.com/menu/201409/329668.shtml').read()
+# text = urlopen('http://www.kekenet.com/Article/201109/153564.shtml').read()
 # c = chardet.detect(text)
 # code = c['encoding']
 # print code
@@ -75,37 +71,37 @@ for page in page_list:
 #     print a
 
 # 爬取文章  图片
-picture_list = []
-j = 0
-for i in story_list:
-    text = urlopen(i).read()
-    text = str(text).replace('\r\n','')
-# print text
-    pic_download = re.search(r'http://pic.kekenet.*?jpg', text)
-    if pic_download != None:
-        pic_download = pic_download.group()
-        if len(pic_download) > 100:
-            a = re.findall(r'http://pic.kekenet.*?jpeg', pic_download)
-            if a == []:
-                b = re.findall(r'http://pic.kekenet.*?JPG', pic_download)
-                if b == []:
-                    pic_download =re.findall(r'http://pic.kekenet.*?png',pic_download)[0]
-                else:
-                    pic_download = b[0]
-            else:
-                pic_download = a[0]
-    else:
-        pic_download = ''
-    print pic_download
-    if pic_download != '':
-        if re.findall('png',pic_download) == []:
-            a = r'/home/carrie/downloads/story/picture/' + str(j) + r'.jpg'
-            urlretrieve(pic_download,a)
-        else:
-            a = r'/home/carrie/downloads/story/picture/' + str(j) + r'.png'
-            urlretrieve(pic_download,a)
-    j = j + 1
-    picture_list.append(pic_download)
+# picture_list = []
+# j = 0
+# for i in story_list:
+#     text = urlopen(i).read()
+#     text = str(text).replace('\r\n','')
+# # print text
+#     pic_download = re.search(r'http://pic.kekenet.*?jpg', text)
+#     if pic_download != None:
+#         pic_download = pic_download.group()
+#         if len(pic_download) > 100:
+#             a = re.findall(r'http://pic.kekenet.*?jpeg', pic_download)
+#             if a == []:
+#                 b = re.findall(r'http://pic.kekenet.*?JPG', pic_download)
+#                 if b == []:
+#                     pic_download =re.findall(r'http://pic.kekenet.*?png',pic_download)[0]
+#                 else:
+#                     pic_download = b[0]
+#             else:
+#                 pic_download = a[0]
+#     else:
+#         pic_download = ''
+#     print pic_download
+#     if pic_download != '':
+#         if re.findall('png',pic_download) == []:
+#             a = r'/home/carrie/downloads/story/picture/' + str(j) + r'.jpg'
+#             urlretrieve(pic_download,a)
+#         else:
+#             a = r'/home/carrie/downloads/story/picture/' + str(j) + r'.png'
+#             urlretrieve(pic_download,a)
+#     j = j + 1
+#     picture_list.append(pic_download)
 
 # pic_download = re.search(r'http://pic.kekenet.*?jpg','http://pic.kekenet///.*?jpg').group()
 # print type(pic_download)
--- aaa.py
+++ aaa.py
@@ -1,12 +1,68 @@
+#coding=utf-8
 __author__ = 'carrie'
-from urllib import urlopen,urlretrieve
-a = r'agdg' + str(1)
-print a
+import re
+from urllib import urlretrieve
+# from urllib import urlopen,urlretrieve
+# a = r'agdg' + str(1)
+# print a
+#
+# a = r'/home/carrie/downloads/story/picture/' + str(1) + r'.png'
+# urlretrieve('http://pic.kekenet.com//2014/0617/51821402998609.png',a)
+#
+# c = urlretrieve('http://pic.kekenet.com/2013/0830/83321377852882.JPG', '/home/carrie/66171411122.jpg')
+# # print c
+# c = urlretrieve('http://pic.kekenet.com//2014/0520/45641400576355.png','/home/carrie/66171411112.png')
+# c = urlretrieve('http://pic.kekenet.com//2014/0627/99731403860110.jpeg','/home/carrie/661714111177.jpg')
 
-a = r'/home/carrie/downloads/story/picture/' + str(1) + r'.png'
-urlretrieve('http://pic.kekenet.com//2014/0617/51821402998609.png',a)
+# page_list = []
+# page_list.append('http://www.kekenet.com/menu/13407/')
+# for i in range(34,34):
+#     http = 'http://www.kekenet.com/menu/13407/List_%s' % i + '.shtml'
+#     # print http
+#     page_list.append(http)
+# print 1, page_list
+# 检测正则中(.*?)与.*?的不同
+# a = '>agsrh<'
+# b = re.compile(r'>(.*?)<')
+# title = re.search(b, a)
+# print 1,title.group()
+# title = re.search(r'>.*?<', a)
+# print 2,title.group()
 
-c = urlretrieve('http://pic.kekenet.com/2013/0830/83321377852882.JPG', '/home/carrie/66171411122.jpg')
+# a = '<a http://www.kekenet.com/menu/201409/329668.shtml \a>'
+# print type(a),a
+# b = re.findall(r'<a.*?\a>',a)
+# c = re.findall(r'<a(.*?)\a>',a)
+# print b
 # print c
-c = urlretrieve('http://pic.kekenet.com//2014/0520/45641400576355.png','/home/carrie/66171411112.png')
-c = urlretrieve('http://pic.kekenet.com//2014/0627/99731403860110.jpeg','/home/carrie/661714111177.jpg')
+
+#检测lrc文件
+# a = 'http://www.kekenet.com/Sound/Article/fangtanlu/20111117.mp3.lrc'
+g = 'http://www.kekenet.com/Sound/2015/11/15_3459987kpP.lrc'
+urlretrieve(g,'/home/carrie/downloads/story/picture/g.lrc')
+
+
+# 检测for循环中列表删除元素后长度是否变化
+# list = ['asd','ad',123,'bgs']
+# print 1, len(list)
+# list.pop(2)
+# print 2, len(list),list
+# for i in range (0,len(list)):
+#     print 3, list[i]
+
+#检测re.spilt的效果
+# str  = 'asd' + '\r\n' +'dfdg' + '\r\n'
+# print str
+# title = re.split('\r\n',str)
+# print title
+
+#字典的用法
+# dict = {'a':['a','b'],'b':[1, 2, 3]}
+# print dict['a']
+
+
+
+
+
+
+
